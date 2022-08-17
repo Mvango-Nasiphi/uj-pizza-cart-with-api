1448ec4cd190a3676a2572302e19553f063032e0
Nasiphi <mvangonasiphi17@gmail.com>
2022-08-17 14:39:37 +0200

    initial commit

---
+++ pizza-cart-api.js
@@ -0,0 +1,133 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzaCartWithAPIWidget', function () {
+        return {
+
+            init() {
+                axios
+                    .get(`https://pizza-cart-api.herokuapp.com/api/pizzas`)
+                    .then((result) => {
+                        const pizzas = result.data.pizzas;
+                        this.pizzas = pizzas;
+                    })
+                    .then(() => {
+                        return this.createCart();
+                    })
+                    .then((result) => {
+                        console.log(result.data);
+                        this.cartId = result.data.cart_code;
+                    })
+
+            },
+            createCart() {
+                // return axios.get('https://pizza-cart-api.herokuapp.com/api/pizza-cart/create?username' + this.username)
+            },
+
+            showCart() {
+                const url = `https://pizza-cart-api.herokuapp.com/api/pizza-cart/${this.cartId}/get`;
+
+                axios
+                    .get(url)
+                    .then((result) => {
+                        this.cart = result.data;
+                    });
+            },
+
+            pizzaImage(pizza) {
+                return `./img/${pizza.size}.png`
+            },
+
+
+
+
+            message: '',
+            username: 'Nasiphi',
+            pizzas: [],
+            cartId: 'ZDopv3XngH',
+            cart: { total: 0 },
+            featured:[],
+            paymentAmount:null,
+            paymentMessage:'',
+            displayCart:false,
+            showCheckout:true,
+            showMessage:false,
+
+            add(pizza) {
+
+                const params = {
+                    cart_code: this.cartId,
+                    pizza_id: pizza.id
+                }
+
+                axios
+                    .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/add', params)
+                    .then(() => {
+                        this.message = "Pizza added to cart"
+                        this.showCart();
+                    })
+                    .catch(err => alert(err));
+
+            },
+            remove(pizza) {
+
+                const params = {
+                    cart_code: this.cartId,
+                    pizza_id: pizza.id
+                }
+
+                axios
+                    .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/remove', params)
+                    .then(() => {
+                        this.message = "Pizza removed from cart"
+                        this.showCart();
+                    })
+                    .catch(err => alert(err));
+
+            },
+
+            featured(){
+                axios
+                    .get('https://pizza-cart-api.herokuapp.com/api/pizzas/featured')
+                    .then((result) =>{
+                        this.featured = result.data.pizzas;
+                    })
+                    .then(()=>{
+                        Object.assign({}, this.featured);
+                    })
+            },
+
+            pay(amount) {
+                const params = {
+                    cart_code: this.cartId,
+                }
+                amount = this.paymentAmount;
+                axios
+                    .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/pay', params)
+                    .then(() =>{
+                        if(!this.paymentAmount){
+                            this.paymentMessage = 'No amount entered';
+                        }
+                        else if(this.paymentAmount >= (this.cart.total)){
+                            this.paymentMessage = 'Enjoy  your pizzas';
+
+                            setTimeout(() => {
+                                this.cart.total = 0;
+                                this.paymentMessege = '';
+                                this.paymentAmount = 0;
+                                this.message = '';
+                                window.location.reload()
+                            }, 3000);
+                            
+                        }
+                        else{
+                            this.paymentMessage = 'Sorry, amount entered not enough'
+                        }
+                    })
+                    .then(() => {
+                        this.showCart();
+                    })
+                    .catch(err => alert(err));
+                //console.log(amount)
+            },
+        }
+    });
+})
